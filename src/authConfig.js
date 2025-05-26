f418371933adaccbbb8a50ee531fd819020b03c3
Seungmuk Chae <csm5042@gmail.com>
2025-05-26 15:47:42 +0900

    fix: re-add loginRequest export

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,11 +1,20 @@
+const isLocalhost = window.location.hostname === "localhost";
+
 export const msalConfig = {
   auth: {
     clientId: "0abca3da-8de2-43d2-8ff3-5a8090162c4c",
     authority: "https://login.microsoftonline.com/shints2.onmicrosoft.com",
-    redirectUri: "https://stellar-sopapillas-adb6a7.netlify.app", // ← 여기에 Netlify 주소 입력
+    redirectUri: isLocalhost
+      ? "http://localhost:3000"
+      : "https://stellar-sopapillas-adb6a7.netlify.app", // ← 실제 Netlify 주소로 교체
   },
   cache: {
     cacheLocation: "localStorage",
     storeAuthStateInCookie: false,
   },
+};
+
+export const loginRequest = {
+  scopes: ["User.Read", "Sites.ReadWrite.All", "openid", "profile"],
+  prompt: "select_account",
 };
