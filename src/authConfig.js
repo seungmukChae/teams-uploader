1855e36af1b5bbdedec531d04b07953c877efa9c
Seungmuk Chae <csm5042@gmail.com>
2025-05-26 15:45:37 +0900

    authConfig 변경

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,16 +1,11 @@
 export const msalConfig = {
   auth: {
     clientId: "0abca3da-8de2-43d2-8ff3-5a8090162c4c",
-    authority: "https://login.microsoftonline.com/shints2.onmicrosoft.com", // 또는 tenant-specific
-    redirectUri: "http://localhost:3000",
+    authority: "https://login.microsoftonline.com/shints2.onmicrosoft.com",
+    redirectUri: "https://stellar-sopapillas-adb6a7.netlify.app", // ← 여기에 Netlify 주소 입력
   },
   cache: {
     cacheLocation: "localStorage",
     storeAuthStateInCookie: false,
   },
-};
-
-export const loginRequest = {
-  scopes: ["User.Read", "Sites.ReadWrite.All", "openid", "profile"], // ✅ 반드시 openid 포함
-  prompt: "select_account",
 };
